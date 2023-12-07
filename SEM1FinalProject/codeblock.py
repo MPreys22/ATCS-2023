2f56e83a328e378556b46ea033bd32707c1b57cf
mikemulla22 <michael.preys@menloschool.org>
2023-12-06 19:47:01 -0800

    Background screen and added music and codeblock

--- SEM1FinalProject/codeblock.py
+++ SEM1FinalProject/codeblock.py
@@ -1,4 +1,8 @@
 import pygame
 
-# class codeblock(pygame.sprite.Sprite):
-#     gfd = 1
+class Codeblock(pygame.sprite.Sprite):
+    def __init__(self, width, height, pos_x, pos_y, color):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface([width, height])
+        self.image.fill(color)
+        self.rect = self.image.get_rect()
--- SEM1FinalProject/game.py
+++ SEM1FinalProject/game.py
@@ -1,38 +1,50 @@
-import pygame 
+import pygame, sys
+from codeblock import Codeblock
 
 class Game:
-    SCREEN_WIDTH = 500
-    SCREEN_HEIGHT = 500
+    SCREEN_WIDTH = 1000
+    SCREEN_HEIGHT = 800
     BACKGROUND = (255, 255, 255)
 
 
-    # def __init__(self):
-    #     # Initialize pygame
-    #     pygame.init()
-    #     pygame.mixer.init()
-    #     self.clock = pygame.time.Clock()
-    #     self.dt = 0
+    def __init__(self):
+        # Initialize pygame
+        pygame.init()
+        pygame.mixer.init()
+        self.clock = pygame.time.Clock()
+        self.codeblock = Codeblock(50,50,100,100,(255,0,0))
+        self.codeblock_group = pygame.sprite.Group()
+        self.codeblock_group.add(self.codeblock)
 
-    # Game Window 
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    screen.fill(BACKGROUND)
+        # Game Window 
+        self.screen = pygame.display.set_mode((self.SCREEN_WIDTH, self.SCREEN_HEIGHT))
+        self.screen.fill(self.BACKGROUND)
 
 
-    # Set the caption of the screen 
-    pygame.display.set_caption('Geeksforgeeks') 
-    
-    # Update the display using flip 
-    pygame.display.flip() 
-    
-    # Variable to keep our game loop running 
-    running = True
-    
-    # game loop 
-    while running: 
-        
-    # for loop through the event queue   
-        for event in pygame.event.get(): 
-        
-            # Check for QUIT event       
-            if event.type == pygame.QUIT: 
-                running = False
+        # Set the caption of the screen 
+        pygame.display.set_caption('CS JOURNEY') 
+        self.background_img = pygame.image.load("Assets/BackGround.jpg")
+        self.intro_music = pygame.mixer.Sound('Assets/Sounds/Music.wav')
+    def run(self):
+        self.intro_music.play()
+
+        # game loop 
+        while True: 
+            # for loop through the event queue   
+            for event in pygame.event.get(): 
+                # Check for QUIT event       
+                if event.type == pygame.QUIT: 
+                    pygame.quit()
+                    sys.exit()
+
+
+            # Update the display using flip 
+            self.screen.blit(self.background_img, (0, 0))
+            self.codeblock_group.draw(self.screen)
+            self.clock.tick(60)
+
+            pygame.display.flip() 
+
+if __name__ == "__main__":
+    g = Game()
+    g.run()
